0476727242714d4ad252ba732576122dbaa21b87
sanaachintya2016 <82821838+sanaachintya2016@users.noreply.github.com>
2021-07-30 10:06:18 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# cake-recipes
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,35 @@
+function setup() {
+  createCanvas(1500,400);
+}
+
+function draw() {
+  background(255,255,255);  
+fill("black")
+text("•400 gms (1 tin)Nestlé MILKMAID" ,190 ,20);
+text("•200 gms Maida (All Purpose Flour)",190,35);
+text("•1 tbsp Cocoa Powder",190,50);
+text("•1 tsp Baking Powder",190,65);
+text("•1 tsp Soda Bicarbonate",190,80);
+text("•100 gms Butter",190,95);
+text("•1 tsp Vanilla Essence",190,110);
+text("•200 ml Aerated Soda",190,125);
+text("•2 tbsp Red Colour",190,140);
+fill("red")
+text("Topping",210,165)
+fill("black")
+text("•100 gmNestlé MILKMAID",190,190)
+fill("black")
+text("•400 gmsNestlé a+ Nourish Dahi",190,205)
+fill("black")
+text("•1 tbsp Butter",190,220)
+fill("blue")
+text("HOW TO MAKE EGGLESS RED VELVET CUP CAKE",100,250)
+fill("black")
+text("• Preheat oven to 180⁰ C. Line the muffin tray with paper cups.",50,275)
+text("• Sieve together maida, cocoa powder, baking powder, and soda bicarbonate. Soften butter in a bowl, add Nestlé MILKMAID, and beat well. Add vanilla essence and mix.",50,290)
+text("• Slowly fold in the sieved maida mixture and aerated soda alternately to the Milkmaid-Butter mix till all the maida mixture and soda are used up. Add the red colour and blend well.",50,305)
+text("• Pour the batter into the paper cups filling ¾ of the cup and bake in the preheated oven for 15-20 minutes or till a toothpick inserted in the center of the cake comes out clean",50,320)
+text("• Remove from oven, cool for a while. Pipe the topping over the cakes and decorate.",50,335)
+text("• Topping: Strain the dahi in a muslin cloth for 20 minutes to get hung curd. Place the hung curd, Nestlé MILKMAID, and butter in a bowl and beat together till thick. Spoon into an icing bag and pipe over the cool cakes.",50,350)
+drawSprites();
+}
